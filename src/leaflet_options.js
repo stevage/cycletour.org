ee8e45ae7cf40dcddf384c1316a54fe6692d3e8b
Steve Bennett <stevage@gmail.com>
2018-06-23 11:07:08 +1000

    cycletour 1

--- src/leaflet_options.js
+++ src/leaflet_options.js
@@ -21,8 +21,8 @@ var streets = L.tileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@
 
 module.exports = {
   defaultState: {
-    center: L.latLng(38.8995, -77.0269),
-    zoom: 13,
+    center: L.latLng(-36.8, 145.8),
+    zoom: 8,
     waypoints: [],
     language: 'en',
     alternative: 0,

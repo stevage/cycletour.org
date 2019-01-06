c6947ed81d8528c58b44c5d68175c70f568f02b3
Steve Bennett <stevage@gmail.com>
2019-01-06 15:41:25 +1100

    Update

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
 // const mapboxgl = require('mapbox-gl');
 import mapboxgl from 'mapbox-gl';
 import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import { MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher/dist/index";
+import "mapbox-gl-style-switcher/styles.css";
+
+// import { MapboxStyleDefinition, MapboxStyleSwitcherControl } from "mapbox-gl-style-switcher";
 export default {
   name: 'app',
   components: {
@@ -28,8 +32,60 @@ function initMap() {
   map.addControl(new MapboxDirections({
       accessToken: mapboxgl.accessToken
   }), 'top-left');
+  
+  const oldCycletour = {
+    "version": 8,
+    "sources": {
+        "mapbox://stevage.2xuscm8g": {
+            "url": "mapbox://stevage.2xuscm8g",
+            "type": "raster",
+            "tileSize": 128
+        }
+    },
+    "sprite": "mapbox://sprites/stevage/cjqkcw8mw2ym32smvt9wsh4c4",
+    "glyphs": "mapbox://fonts/stevage/{fontstack}/{range}.pbf",
+    "layers": [
+        {
+            "id": "background",
+            "type": "background",
+            "paint": {
+                "background-color": "rgba(0,0,0,0)"
+            }
+        },
+        {
+            "type": "raster",
+            "paint": {
+                "raster-resampling": "nearest",
+            },
+            "layout": {},
+            "id": "stevage-2xuscm8g",
+            "source": "mapbox://stevage.2xuscm8g"
+        }
+    ]
+}
+
+
+  const styles = [
+    {
+        title: "Outdoors",
+        uri:'mapbox://styles/stevage/cjopcl1j9555x2rt298enfwhf'
+    },
+    {
+        title: "Light",
+        uri:"mapbox://styles/mapbox/light-v9"
+    }, {
+      title: 'Old-cycletour',
+      uri: oldCycletour
+    }
+];
+
+  map.addControl(new MapboxStyleSwitcherControl(styles));
   window.map = map;
 }
+
+/*
+Can get route geojson: map.getSource('directions')._data
+*/
 
 require('../node_modules/mapbox-gl/dist/mapbox-gl.css');
 require('../node_modules/@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css');

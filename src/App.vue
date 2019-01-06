42c0643a3135e1b342b669158f7d3c65b994ec13
Steve Bennett <stevage@gmail.com>
2019-01-06 13:11:03 +1100

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id='map'></div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// const mapboxgl = require('mapbox-gl');
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  }, mounted() {
+    initMap();
   }
 }
+
+function initMap() {
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic3RldmFnZSIsImEiOiJGcW03aExzIn0.QUkUmTGIO3gGt83HiRIjQw';
+  var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/stevage/cjopcl1j9555x2rt298enfwhf',
+      center: [148,-36],
+      zoom: 11
+  });
+  map.addControl(new MapboxDirections({
+      accessToken: mapboxgl.accessToken
+  }), 'top-left');
+  window.map = map;
+}
+
+require('../node_modules/mapbox-gl/dist/mapbox-gl.css');
+require('../node_modules/@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css');
+
 </script>
 
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+body { margin:0; padding:0; }
+#map { position:absolute; top:0; bottom:0; width:100%; }
+
 </style>
